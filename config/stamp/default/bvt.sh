c71982e4d45c80b05ca016727ae5226bbaf342e8
unknown <aizel@cloudswyft.com>
2018-05-18 03:44:16 +0800

    Modfied with new SSH Key and URLs for redeployment 	modified:   config/stamp/default/bvt.sh 	modified:   config/stamp/default/id_rsa 	modified:   config/stamp/default/id_rsa.pub `	modified:   config/stamp/default/parameters.json

--- config/stamp/default/bvt.sh
+++ config/stamp/default/bvt.sh
@@ -17,8 +17,8 @@ ENVIRONMENT=bvt
 
 # the site certificate files (.crt & .key) have to be named after the BASE_URL value
 BASE_URL=cloudswyft.com 
-LMS_URL=epacademy-official.$BASE_URL
-CMS_URL=epacademy-studio.$BASE_URL
+LMS_URL=epacademy-master.$BASE_URL
+CMS_URL=epacademy-learn.$BASE_URL
 PREVIEW_URL=preview.$BASE_URL
 
 # deployment configuration
